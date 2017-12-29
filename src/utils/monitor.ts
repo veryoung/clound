a7c78e7fda6f76621c57014141a2248dcfc89c51
wjt <wujiantao@swad.com>
2017-12-29 13:54:52 +0800

    修改login页面死循环的BUG

--- src/utils/monitor.ts
+++ src/utils/monitor.ts
@@ -8,13 +8,16 @@ export class UserStatus {
     constructor(next: Function) {
         GeneralServer.oneself().then((response: AxiosResponse<ResType>) => {
             let res: ResType = response.data;
+            if (res.data.uid === "") {
+                next("/login");
+                return;
+            }
             switch (res.status) {
                 case "suc":
                     Store.dispatch(USER.DEFAULTUSER, { uid: res.data.uid });
                     next("/home");
                     break;
                 default:
-                    next("/login");
                     break;
             }
         });
