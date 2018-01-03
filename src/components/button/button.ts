a4edccc2889482ef7b55fcdc7007f9caf2adfc05
wanyang <675874828@qq.com>
2018-01-03 17:22:54 +0800

    合并冲突

---
+++ src/components/button/button.ts
@@ -0,0 +1,10 @@
+import Component from "vue-class-component";
+import Vue from "vue";
+
+@Component({
+    name: "Btn",
+    template: require("./button.html")
+})
+export class Btn extends Vue {
+
+}
