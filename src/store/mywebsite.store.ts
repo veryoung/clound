da1231265ffe51d350dc7e86f9ebb01ba8ef5459
wjt <wujiantao@swad.com>
2018-01-03 18:28:25 +0800

    增加 我的网站模型

--- src/store/mywebsite.store.ts
+++ src/store/mywebsite.store.ts
@@ -1,5 +1,5 @@
 import { Module } from "vuex";
-import { MYWEBSITEEVENT } from "@store/mywebsite.type";
+import { MYWEBSITEEVENT, MyWebsiteType } from "@store/mywebsite.type";
 import { TableServer } from "@server/table";
 import { MywebsiteServer } from "@server/mywebsite";
 import { AxiosResponse } from "axios";
@@ -12,18 +12,93 @@ import { TABLECONFIG } from "@store/table.type";
 
 
 
-export const OrganizationStore: Module<OrganizationType, any> = {
-    state: (): OrganizationType => {
+export const OrganizationStore: Module<MyWebsiteType, any> = {
+    state: (): MyWebsiteType => {
         return {
+            websiteMessage: {
+                "init": {
+                    id: "",
+                    // string	@mock=bcf30118.waf
+                    cname: "",
+                    // string	@mock=17-12-19 15:48:51
+                    domain: "",
+                    // 	
+                    service: {
+                        cdn_enable: "",
+                        ads_enable: "",
+                        mirror_enable: "",
+                        waf_enable: ""
+                    },
+                    // array<number>	@mock=80
+                    http_port: [0],
+                    // array<number>	@mock=443
+                    https_port: [0],
+                    // string;
+                    label: "",
+                    organization: "",
+                    source_info: "",
+                    source_type: "",
+                }
+            },
+            tableData: {
+                "": [{
+                    // cname别名	string	
+                    cname: "",
+                    // 创建人
+                    cperson: "",
+                    // 添加日期	string	yyyymmdd	
+                    ctime: "",
+                    // 网站域名	string	@mock=test.test.com
+                    domain: "",
+                    // 网站id
+                    id: "",
+                    // 可删除		0-关闭;1-开启	
+                    is_delete: "",
+                    // 可编辑		0-关闭;1-开启
+                    is_update: "",
+                    // 网站名称	string	@mock=test_name
+                    label: "",
+                    // 防御状态		直接显示
+                    open_waf: "",
+                    // 	所属组织	string	@mock=org
+                    organization: "",
+                    // 回源地址	array < string >	@mock=www.baidu.com
+                    source_info: [""],
+                    // 回源方式	string	@mock=CNAME
+                    source_type: "",
+                    // 	接入状态		直接显示
+                    state: "",
+                    port: {
+                        http: [0],
+                        https: [0]
+                    },
+                    service: {
+                        cdn_enable: "",
+                        ads_enable: "",
+                        mirror_enable: "",
+                        waf_enable: ""
+                    }
+                }]
+            }
         };
     },
 
     mutations: {
         [MYWEBSITEEVENT.GETLISTMESSAGE]: (state, payload) => {
+            if (!state.tableData[Math.floor(payload.page) - 1]) {
+                state.tableData[Math.floor(payload.page) - 1] = [];
+            }
+            state.tableData[Math.floor(payload.page) - 1] = [].concat(payload.message);
+        },
+        [MYWEBSITEEVENT.GETWEBMESSAGE]: (state, payload) => {
+            state.websiteMessage[payload.website_id] = payload.message;
         },
     },
     actions: {
         [MYWEBSITEEVENT.GETLISTMESSAGE]: ({ state, commit, rootState }, payload) => {
+            if ((Math.floor(payload.page) - 1) in state.tableData) {
+                EventBus.doNotify(CONSTANT.GETLISTMESSAGE);
+            }
             MywebsiteServer.getList(payload).then((response: AxiosResponse<ResType>) => {
                 let res: ResType = response.data;
                 switch (res.status) {
@@ -37,8 +112,29 @@ export const OrganizationStore: Module<OrganizationType, any> = {
                 }
             });
         },
+        [MYWEBSITEEVENT.GETWEBMESSAGE]: ({ state, commit, rootState }, payload) => {
+            if (payload.website_id in state.websiteMessage) {
+                EventBus.doNotify(CONSTANT.GETWEBMESSAGE, { website_id: payload.website_id });
+            }
+            MywebsiteServer.getWebsiteDetail(payload.website_id).then((response: AxiosResponse<ResType>) => {
+                let res: ResType = response.data;
+                switch (res.status) {
+                    case "suc":
+                        commit(MYWEBSITEEVENT.GETWEBMESSAGE, { website_id: payload.website_id, message: res.data });
+                        EventBus.doNotify(CONSTANT.GETLISTMESSAGE, { website_id: payload.website_id });
+                        break;
+                    default:
+                        break;
+                }
+            });
+        }
     },
     getters: {
-
+        "websiteMessage": function (state) {
+            return state.websiteMessage;
+        },
+        "tableData": function (state) {
+            return state.tableData;
+        }
     }
 };
--- src/store/mywebsite.type.ts
+++ src/store/mywebsite.type.ts
@@ -1,9 +1,84 @@
 import { EventType } from "@store/store";
 
 export const MYWEBSITEEVENT: EventType = {
-    GETLISTMESSAGE: "获取我的网站列表数据"
+    GETLISTMESSAGE: "获取我的网站列表数据",
+    GETWEBMESSAGE: "获取网站详情"
 };
 
-interface MyWebsiteType {
-    
+export interface MyWebsiteType {
+    tableData: TableType;
+    websiteMessage: WebType;
+}
+
+interface WebType {
+    [extra: string]: WebMessageType;
+}
+interface WebMessageType {
+    id?: string;
+    // string	@mock=bcf30118.waf
+    cname: string;
+    // string	@mock=17-12-19 15:48:51
+    domain: string;
+    // 	
+    service: ServerType;
+    // array<number>	@mock=80
+    http_port: number[];
+    // array<number>	@mock=443
+    https_port: number[];
+    // string;
+    label: string;
+    organization: string;
+    source_info: string;
+    source_type: string;
+}
+
+interface TableType {
+    [extra: string]: TableCloums[];
+}
+interface TableCloums {
+    // cname别名	string	
+    cname: string;
+    // 创建人
+    cperson: string;
+    // 添加日期	string	yyyymmdd	
+    ctime: string;
+    // 网站域名	string	@mock=test.test.com
+    domain: string;
+    // 网站id
+    id: string;
+    // 可删除		0-关闭;1-开启	
+    is_delete: string;
+    // 可编辑		0-关闭;1-开启
+    is_update: string;
+    // 网站名称	string	@mock=test_name
+    label: string;
+    // 防御状态		直接显示
+    open_waf: string;
+    // 	所属组织	string	@mock=org
+    organization: string;
+    // 回源地址	array < string >	@mock=www.baidu.com
+    source_info: string[];
+    // 回源方式	string	@mock=CNAME
+    source_type: string;
+    // 	接入状态		直接显示
+    state: string;
+    port: PortType;
+    service: ServerType;
+}
+
+interface ObjType {
+    [extra: string]: string;
+}
+interface PortType {
+    // http端口	array < number > [80, 8081]
+    http: number[];
+    // https端口	array < number > [443]
+    https: number[];
+}
+interface ServerType {
+    // cc / ddos 防御		0 - 关闭; 1 - 开启
+    cdn_enable: string;
+    ads_enable: string;
+    mirror_enable: string;
+    waf_enable: string;
 }
