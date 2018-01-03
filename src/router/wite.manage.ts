b9e81696158934a640d7421760c0fe051b09cddc
wanyang <675874828@qq.com>
2018-01-03 18:51:23 +0800

    搭建好了 我的网站的路由

---
+++ src/router/wite.manage.ts
@@ -0,0 +1,16 @@
+import { WiteManagement } from "@views/witemanage/wite.manage";
+import Router from "vue-router";
+import { RouteConfig } from "vue-router";
+
+
+
+export const WiteManageRouter: RouteConfig[] = [
+    {
+        path: "myWite",
+        name: "我的网站",
+        meta: {
+            icon: "icon-yonghuguanli",
+        },
+        component: WiteManagement,
+    },
+];
