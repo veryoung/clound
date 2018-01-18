ad85c1dd84b7fb52b84f7b887a1ac7a6da5fa339
wjt <wujiantao@swad.com>
2018-01-18 16:05:24 +0800

    增加柱状图

--- src/components/echarts/bar/bar.ts
+++ src/components/echarts/bar/bar.ts
@@ -1,6 +1,49 @@
-import Vue from "vue";
 import Component from "vue-class-component";
+import { chartConfig, ChartComponent } from "@components/echarts/default.chart";
 
 
 
-export class Bar extends Vue{}
+const timestamp: string = new Date().getTime() + "";
+@chartConfig({
+    tooltip: {
+        trigger: "item",
+        formatter: "{a} <br/>{b} : {c} ({d}%)"
+    },
+    legend: {
+        type: "plain",
+        data: ["初始化"]
+    },
+    xAxis: {
+        data: ["初始化数据1", "初始化数据2", "初始化数据3", "初始化数据4", "初始化数据5", "初始化数据6"]
+    },
+    yAxis: {
+        show: true,
+        type: "category"
+    },
+    series: [{
+        name: "初始化",
+        type: "bar",
+        data: [5, 20, 36, 10, 10, 20],
+        tooltip: {
+            formatter: "{a}<br />{b}<br />{c}"
+        }
+    }]
+})
+@Component({
+    name: "barComponent",
+    template: require("./bar.html"),
+    props: {
+        option: {
+            type: Object
+        },
+        id: {
+            type: String,
+            default: `bar-cloud${timestamp}`
+        }
+    }
+})
+export class BarComponent extends ChartComponent {
+    mounted() {
+        this.init();
+    }
+}
