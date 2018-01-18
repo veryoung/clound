4ddce661ad3a812fcb28ec94e5970cef56c71f9e
wjt <wujiantao@swad.com>
2018-01-18 14:56:47 +0800

    删除了一些默认配置

--- src/components/echarts/pie/pie.ts
+++ src/components/echarts/pie/pie.ts
@@ -5,11 +5,6 @@ import { chartConfig, ChartComponent } from "@components/echarts/default.chart";
 require("./pie.styl");
 
 @chartConfig({
-    title: {
-        text: "南丁格尔玫瑰图",
-        subtext: "纯属虚构",
-        x: "center"
-    },
     tooltip: {
         trigger: "item",
         formatter: "{a} <br/>{b} : {c} ({d}%)"
@@ -18,19 +13,6 @@ require("./pie.styl");
         x: "center",
         y: "bottom",
         data: ["rose1", "rose2", "rose3", "rose4", "rose5", "rose6", "rose7", "rose8"]
-    },
-    toolbox: {
-        show: true,
-        feature: {
-            mark: { show: true },
-            dataView: { show: true, readOnly: false },
-            magicType: {
-                show: true,
-                type: ["pie", "funnel"]
-            },
-            restore: { show: true },
-            saveAsImage: { show: true }
-        }
     },
     calculable: true,
     series: [
