cfa090f56230757311e6fee8d0f4a89ae160977a
wjt <wujiantao@swad.com>
2018-01-20 13:10:45 +0800

    CWF-1034 网站编辑-创建网站的时候是选中http与https，编辑的时候去掉https，点击保存，https还是存在

--- src/views/websitemanage/website.operation/website.operation.ts
+++ src/views/websitemanage/website.operation/website.operation.ts
@@ -320,18 +320,18 @@ export class WebsiteOperation extends Vue {
             } else if (this.form.source_type === "回源域名") {
                 this.form.source_type = "CNAME";
             }
+            if (this.httpsTpye === false) {
+                this.form.https_port = [];
+            }
+            if (this.httpTpye === false) {
+                this.form.http_port = [];
+            }
+            let httpsTemp: any = "";
+            httpsTemp = this.form.https_port;
+            this.form.http_port = this.httpTags;
+            this.form.https_port = this.httpsTags;
             switch (this.operation) {
                 case "add":
-                    let httpsTemp: any = "";
-                    httpsTemp = this.form.https_port;
-                    this.form.http_port = this.httpTags;
-                    this.form.https_port = this.httpsTags;
-                    if (this.httpsTpye === false) {
-                        this.form.https_port = [];
-                    }
-                    if (this.httpTpye === false) {
-                        this.form.http_port = [];
-                    }
                     MywebsiteServer.addWebsite(this.form).then((response: AxiosResponse<ResType>) => {
                         let res: ResType = response.data;
                         switch (res.status) {
@@ -365,6 +365,7 @@ export class WebsiteOperation extends Vue {
                                     message: "编辑网站成功",
                                     type: "success"
                                 });
+                                this.form.https_port = httpsTemp;
                                 this.$router.push("/WebsiteManagement/myWebsite");
                                 break;
                             default:
