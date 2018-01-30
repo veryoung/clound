6ee6bca9f3f64c985d9dbb0f1da5c9608f8ee169
wjt <wujiantao@swad.com>
2018-01-30 11:05:47 +0800

    增加报告图表

--- src/views/reportmanage/preview.report/preview.report.ts
+++ src/views/reportmanage/preview.report/preview.report.ts
@@ -1,12 +1,33 @@
 import Component from "vue-class-component";
 import Vue from "vue";
+import { BarComponent } from "@components/echarts/bar/bar";
+import { PieComponent } from "@components/echarts/pie/pie";
+import { LineComponent } from "@components/echarts/line/line";
+import * as config from "./preview.report.attachment";
+
+
+
 
 
 require("./preview.report.styl");
 @Component({
     name: "PreviewReport",
-    template: require("./preview.report.html")
+    template: require("./preview.report.html"),
+    components: {
+        BarComponent, PieComponent, LineComponent
+    }
 })
 export class PreviewReport extends Vue {
+    // init computed
+    get barConfig() {
+        return config.barConfig;
+    }
 
+    get lineConfig() {
+        return config.lineConfig;
+    }
+
+    get pieConfig() {
+        return config.pieConfig;
+    }
 }
