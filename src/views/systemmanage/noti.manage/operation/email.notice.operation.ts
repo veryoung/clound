b16a965abc2a4c150fe1d295ab176e7e6a45cfa4
wanyang <675874828@qq.com>
2018-02-01 17:19:34 +0800

    合并代码

--- src/views/systemmanage/noti.manage/operation/email.notice.operation.ts
+++ src/views/systemmanage/noti.manage/operation/email.notice.operation.ts
@@ -26,7 +26,7 @@ require("./email.notice.operation.styl");
     components: {
         ModuleTitle,
         RichTextEditor,
-        EmailDiploma 
+        EmailDiploma
     }
 })
 
@@ -46,19 +46,23 @@ export class EmailNoiceOperation extends Vue {
     public chosePerson: Array<string> = [];
 
     // 标题
-    public titles: string[] = ["写短信"];
+    public titles: string[] = ["写邮件"];
 
     // 选择用户
     public dialogVisibleDiploma: boolean = false;
 
     // 表单验证
     public rules: FormRuleType = {
-        title: [
-            { required: true, message: "请填写短信标题", trigger: "blur" },
+        object: [
+            { required: true, message: "请填写邮件标题", trigger: "blur" },
             { min: 1, max: 40, message: "不符合字符规范，字符长度1-40字符", trigger: "blur" }
         ],
         content: [
-            { required: true, message: "请添加短信内容", trigger: "blur" },
+            { required: true, message: "请添加邮件内容", trigger: "blur" },
+            { min: 1, max: 1000, message: "不符合字符规范，字符长度1-1000字符", trigger: "blur" }
+        ],
+        receiver_ids: [
+            { required: true, message: "请选择收件人", trigger: "blur" },
         ],
     };
 
@@ -77,7 +81,7 @@ export class EmailNoiceOperation extends Vue {
 
     getUser(val: Array<object>) {
         let valArray: any = val;
-        for ( let key in valArray) {
+        for (let key in valArray) {
             this.form.receiver_ids.push(valArray[key].uid);
         }
         this.num = valArray.length;
@@ -86,28 +90,33 @@ export class EmailNoiceOperation extends Vue {
     }
 
     submitForm(formBasic: string) {
-        console.log(this.form);
-        NoticeServer.sendMessage(this.form).then((response: AxiosResponse<ResType>) => {
-            let res: ResType = response.data;
-            switch (res.status) {
-                case "suc":
-                    this.$message({
-                        message: "短信填写成功",
-                        type: "success"
-                    });
-                    this.$router.push("/SystemManagement/ReportManagement/emaillnotice");
-                    break;
-                case "error":
-                    this.$message({
-                        message: res.message || "短信填写失败",
-                        type: "error"
-                    });
-                    break;
-                default:
-                    break;
-            }
-
+        let temp: any = this.$refs.noticeform;
+        let flag: boolean = false;
+        temp.validate((valid: any) => {
+            flag = valid;
         });
+        if (flag) {
+            NoticeServer.sendMessage(this.form).then((response: AxiosResponse<ResType>) => {
+                let res: ResType = response.data;
+                switch (res.status) {
+                    case "suc":
+                        this.$message({
+                            message: "邮件填写成功",
+                            type: "success"
+                        });
+                        this.$router.push("/SystemManagement/ReportManagement/emaillnotice");
+                        break;
+                    case "error":
+                        this.$message({
+                            message: res.message || "邮件填写失败",
+                            type: "error"
+                        });
+                        break;
+                    default:
+                        break;
+                }
+            });
+        }
     }
     select() {
         this.dialogVisibleDiploma = true;
--- src/views/systemmanage/noti.manage/operation/message.notice.operation.ts
+++ src/views/systemmanage/noti.manage/operation/message.notice.operation.ts
@@ -52,12 +52,13 @@ export class MessageNoiceOperation extends Vue {
 
     // 表单验证
     public rules: FormRuleType = {
-        title: [
-            { required: true, message: "请填写短信标题", trigger: "blur" },
-            { min: 1, max: 40, message: "不符合字符规范，字符长度1-40字符", trigger: "blur" }
+        receiver_ids: [
+            { required: true, message: "请选择收件人", trigger: "blur" },
         ],
         content: [
             { required: true, message: "请添加短信内容", trigger: "blur" },
+            { min: 1, max: 1000, message: "不符合字符规范，字符长度1-1000字符", trigger: "blur" }
+
         ],
     };
 
@@ -76,7 +77,7 @@ export class MessageNoiceOperation extends Vue {
 
     getUser(val: Array<object>) {
         let valArray: any = val;
-        for ( let key in valArray) {
+        for (let key in valArray) {
             this.form.receiver_ids.push(valArray[key].uid);
         }
         this.num = valArray.length;
@@ -84,28 +85,34 @@ export class MessageNoiceOperation extends Vue {
         this.closeDiploma();
     }
 
-    submitForm(formBasic: string) {
-        NoticeServer.sendMessage(this.form).then((response: AxiosResponse<ResType>) => {
-            let res: ResType = response.data;
-            switch (res.status) {
-                case "suc":
-                    this.$message({
-                        message: "短信填写成功",
-                        type: "success"
-                    });
-                    this.$router.push("/SystemManagement/ReportManagement/messagenotice");
-                    break;
-                case "error":
-                    this.$message({
-                        message: res.message || "短信填写失败",
-                        type: "error"
-                    });
-                    break;
-                default:
-                    break;
-            }
-
+    submitForm() {
+        let temp: any = this.$refs.noticeform;
+        let flag: boolean = false;
+        temp.validate((valid: any) => {
+            flag = valid;
         });
+        if (flag) {
+            NoticeServer.sendMessage(this.form).then((response: AxiosResponse<ResType>) => {
+                let res: ResType = response.data;
+                switch (res.status) {
+                    case "suc":
+                        this.$message({
+                            message: "短信填写成功",
+                            type: "success"
+                        });
+                        this.$router.push("/SystemManagement/ReportManagement/messagenotice");
+                        break;
+                    case "error":
+                        this.$message({
+                            message: res.message || "短信填写失败",
+                            type: "error"
+                        });
+                        break;
+                    default:
+                        break;
+                }
+            });
+        }
     }
     select() {
         this.dialogVisibleDiploma = true;
