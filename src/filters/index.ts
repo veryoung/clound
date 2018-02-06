79930a62e81eca3c2863ee450e8af701b3284ed6
wanyang <675874828@qq.com>
2018-02-06 14:47:06 +0800

    合并代码

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,21 +1,15 @@
 import { DateFilter } from "@filters/date";
 import { NoneFilter } from "@filters/none";
 import { DateTimeFilter } from "@filters/datetime";
-<<<<<<< HEAD
 import { StatusFilter } from "@filters/status";
-=======
 import { OtherFilter } from "@filters/other";
->>>>>>> development
 
 export class InitFilters {
     constructor() {
         new DateFilter();
         new NoneFilter();
         new DateTimeFilter();
-<<<<<<< HEAD
         new StatusFilter();  
-=======
         new OtherFilter();
->>>>>>> development
     }
 }
