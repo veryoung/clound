2f7fefee5291739d2d21778afbf22033b0021a3a
wjt <wujiantao@swad.com>
2018-02-06 11:15:02 +0800

    修正BUG CWF-1088 发送方式-应该为系统发送，非人工发送

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,11 +1,13 @@
 import { DateFilter } from "@filters/date";
 import { NoneFilter } from "@filters/none";
 import { DateTimeFilter } from "@filters/datetime";
+import { OtherFilter } from "@filters/other";
 
 export class InitFilters {
     constructor() {
         new DateFilter();
         new NoneFilter();
         new DateTimeFilter();
+        new OtherFilter();
     }
 }
