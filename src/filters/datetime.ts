ff0aaee7949128461c19251681e378a676666a4f
wanyang <675874828@qq.com>
2018-03-06 10:31:11 +0800

    CWF-1138

--- src/filters/datetime.ts
+++ src/filters/datetime.ts
@@ -8,7 +8,7 @@ export class DateTimeFilter {
             "datetime",
             function (value: string) {
                 // 将YYYYMMDDHHmmss传入格式处理为 YYYYMMDD HHmmss
-                if (value === "") return value;
+                if (value === "" || !value) return value;
                 let tmp = value.substring(0, 8);
                 let estr = value.substring(8, value.length);
                 value = tmp + " " + estr;
